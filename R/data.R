037e370cd24227bd03e27a1f0b72f5236657bfc5
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2014-03-10 09:26:34 +0000

    added hreidar, amended to foreign

---
+++ R/data.R
@@ -0,0 +1,98 @@
+#' @name hreidar
+#' @title Some title
+#' @description Some description
+#' @docType data
+#' @usage Some usage
+#' @format Some format
+#' @source Some source
+#' @author Some author
+NULL
+
+#' @name landings_pax
+#' @title Some title
+#' @description Some description
+#' @docType data
+#' @usage Some usage
+#' @format Some format
+#' @source Some source
+#' @author Some author
+NULL
+
+#' @name afliFiskistofa
+#' @title Some title
+#' @description Some description
+#' @docType data
+#' @usage Some usage
+#' @format Some format
+#' @source Some source
+#' @author Some author
+NULL
+
+#' @name foreign
+#' @title Some title
+#' @description Some description
+#' @docType data
+#' @usage Some usage
+#' @format Some format
+#' @source Some source
+#' @author Some author
+NULL
+
+#' @name asfis
+#' @title Some title
+#' @description Some description
+#' @docType data
+#' @usage Some usage
+#' @format Some format
+#' @source Some source
+#' @author Some author
+NULL
+
+#' @name countries
+#' @title Some title
+#' @description Some description
+#' @docType data
+#' @usage Some usage
+#' @format Some format
+#' @source Some source
+#' @author Some author
+NULL
+
+#' @name ices19031949
+#' @title Some title
+#' @description Some description
+#' @docType data
+#' @usage Some usage
+#' @format Some format
+#' @source Some source
+#' @author Some author
+NULL
+
+#' @name ices19502010
+#' @title Some title
+#' @description Some description
+#' @docType data
+#' @usage Some usage
+#' @format Some format
+#' @source Some source
+#' @author Some author
+NULL
+
+#' @name lices
+#' @title Some title
+#' @description Some description
+#' @docType data
+#' @usage Some usage
+#' @format Some format
+#' @source Some source
+#' @author Some author
+NULL
+#' @name ices19852012
+#' @title Some title
+#' @description Some description
+#' @docType data
+#' @usage Some usage
+#' @format Some format
+#' @source Some source
+#' @author Some author
+NULL
