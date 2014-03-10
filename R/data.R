3294c069ce6af2b59891481ca498e29ad7590bc1
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2014-03-10 09:26:34 +0000

    initial seeding

---
+++ R/data.R
@@ -0,0 +1,78 @@
+#' @name afliFiskistofa
+#' @title Some title
+#' @description Some description
+#' @docType data
+#' @usage Some usage
+#' @format Some format
+#' @source Some source
+#' @author Some author
+NULL
+
+#' @name foreign
+#' @title Some title
+#' @description Some description
+#' @docType data
+#' @usage Some usage
+#' @format Some format
+#' @source Some source
+#' @author Some author
+NULL
+
+#' @name asfis
+#' @title Some title
+#' @description Some description
+#' @docType data
+#' @usage Some usage
+#' @format Some format
+#' @source Some source
+#' @author Some author
+NULL
+
+#' @name countries
+#' @title Some title
+#' @description Some description
+#' @docType data
+#' @usage Some usage
+#' @format Some format
+#' @source Some source
+#' @author Some author
+NULL
+
+#' @name ices19031949
+#' @title Some title
+#' @description Some description
+#' @docType data
+#' @usage Some usage
+#' @format Some format
+#' @source Some source
+#' @author Some author
+NULL
+
+#' @name ices19502010
+#' @title Some title
+#' @description Some description
+#' @docType data
+#' @usage Some usage
+#' @format Some format
+#' @source Some source
+#' @author Some author
+NULL
+
+#' @name lices
+#' @title Some title
+#' @description Some description
+#' @docType data
+#' @usage Some usage
+#' @format Some format
+#' @source Some source
+#' @author Some author
+NULL
+#' @name ices19852012
+#' @title Some title
+#' @description Some description
+#' @docType data
+#' @usage Some usage
+#' @format Some format
+#' @source Some source
+#' @author Some author
+NULL
---
+++ R/landr.R
@@ -0,0 +1,10 @@
+#' @name landr-package
+#' @aliases landr
+#' @aliases landrLibrary
+#' @docType package
+#' @title   Landings data from disparate source as R-binaries
+#' @description More about what it does (maybe more than one line)
+#' @details Version 0.1.0: 7. March 2014
+#' @author Various folks at MRI
+#' @seealso link to other documents
+NULL
